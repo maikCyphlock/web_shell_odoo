37a701b032bd7afa967aee6187d5e9da1f250c2f
maikCyphlock <maikolaguilar656@gmail.com>
2025-12-30 18:29:52 -0400

    chore: update authorship to maikCyphlock in log viewer components

--- web_shell/static/src/components/log_viewer/log_main.js
+++ web_shell/static/src/components/log_viewer/log_main.js
@@ -1,7 +1,7 @@
 /** @odoo-module **/
 /*
     Part of Web Shell. See LICENSE file for full copyright and licensing details.
-    Created by MAIKOL AGUILAR (https://github.com/maikol-aguilar)
+    Created by MAIKOL AGUILAR (https://github.com/maikCyphlock)
 */
 import { Component, useState, onMounted } from "@odoo/owl";
 import { registry } from "@web/core/registry";
